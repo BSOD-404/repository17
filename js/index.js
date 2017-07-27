d2f3daac20a5bc681e7eedb72b9b8ea5859e9d0a
Bohdan Melenchuk <jailedkurtanogle@gmail.com>
2017-07-27 23:38:07 +0000

    changes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
-
-
-function fiboNum(num) {
-    if (num ==0) {
-        return 0;
+var fiboNacci = function(a) {
+    if (a === 1) 
+    
+    {
+        return [1, 1];
     }
     
-     if (num ==1) {
-        return 1;
+    else 
+    {
+        var n = fiboNacci(a - 1);
+        n.push(n[n.length - 1] + n[n.length - 2]);
+        return n;
     }
-    
-    //num > 1
-    
-    return fiboNum(num - 2) + fiboNum(num -1);
-}
+};
+
+console.log(fiboNacci(404));
+
+var number = parseInt(prompt('Enter your "N" value you want to find here, please:')); //don't really know what the radix parameter is missing here
+alert(fiboNacci(number));
